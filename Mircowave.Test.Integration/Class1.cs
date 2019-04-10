f51417da2b2e9fcc19a1c7aceb2cc3a143c37e6c
Sarahkfenger <43434838+Sarahkfenger@users.noreply.github.com>
2019-04-10 11:14:00 +0200

    Oprettelse af integrationtest projekt

---
+++ Mircowave.Test.Integration/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Mircowave.Test.Integration
+{
+   [TestFixture]
+    public class Class1
+    {
+
+       public void SetUp()
+       {
+
+       }
+    }
+}
